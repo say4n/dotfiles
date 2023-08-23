b4fe0f65e89dd628e3ea3c94b4d3fd5052776270
Sayan Goswami <goswami.sayan47@gmail.com>
2023-08-23 23:49:25 +0100

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ alias p="cd ~/Projects"
 
 alias vpn_deploy='aws ec2 run-instances --launch-template LaunchTemplateId=lt-0f4a2456f4d641c58'
 alias vpn_destroy='aws ec2 terminate-instances --instance-ids (aws ec2 describe-instances | jq -r ".Reservations[].Instances[] | .InstanceId")'
-alias vpn_ssh='aws ec2-instance-connect ssh --instance-id (aws ec2 describe-instances | jq -r ".Reservations[].Instances[] | .InstanceId") --os-user ubuntu'
+alias vpn_ssh='aws ec2-instance-connect ssh --instance-id (aws ec2 describe-instances | jq -r \'.Reservations[].Instances[] | select(.State.Name == "running") | .InstanceId\') --os-user ubuntu'
 alias vpn_list='aws ec2 describe-instances | jq -r ".Reservations[].Instances[] | .InstanceId, .State.Name"'
 
 # Paths.
