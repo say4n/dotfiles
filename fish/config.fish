fb979b14e72c1fb1460738f917948e717c031673
Sayan Goswami <goswami.sayan47@gmail.com>
2023-08-23 23:54:36 +0100

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-# Set language to English.
+ Set language to English.
 set -x LANG en_US.UTF-8
 
 # Aliases.
@@ -7,7 +7,7 @@ alias p="cd ~/Projects"
 alias vpn_deploy='aws ec2 run-instances --launch-template LaunchTemplateId=lt-0f4a2456f4d641c58'
 alias vpn_destroy='aws ec2 terminate-instances --instance-ids (aws ec2 describe-instances | jq -r ".Reservations[].Instances[] | .InstanceId")'
 alias vpn_ssh='aws ec2-instance-connect ssh --instance-id (aws ec2 describe-instances | jq -r \'.Reservations[].Instances[] | select(.State.Name == "running") | .InstanceId\') --os-user ubuntu'
-alias vpn_list='aws ec2 describe-instances | jq -r ".Reservations[].Instances[] | .InstanceId, .State.Name"'
+alias vpn_list='aws ec2 describe-instances | jq -r ".Reservations[].Instances[] | {InstanceId:.InstanceId, State:.State.Name}"'
 
 # Paths.
 fish_add_path /opt/homebrew/sbin
